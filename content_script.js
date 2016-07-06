ab95541d51167db6615c149d6e840812bd8cbae0
Amit Mondal <axm4814@rit.edu>
2016-07-06 05:07:14 -0400

    get video duration / current time
    able to track video stats via content script

--- content_script.js
+++ content_script.js
@@ -15,3 +15,15 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 		parentElement.insertBefore(newdiv, firstChild);
 	}
 });
+
+// due to changes in manifest.json (run_at: document_idle), this will load
+// BEFORE "load" event, but after DOM is ready!
+// this solves the issue of it not being able to find the video element in some cases.
+var video = document.getElementsByTagName('video')[0];
+window.setInterval(function() {
+  if (video) {
+    if (!video.paused) {
+      console.log(new Date(video.currentTime * 1000).toISOString().substr(11, 8));
+    }
+  }
+}, 1000);
