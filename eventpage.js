299a753df6ab50731138070bc4e74b40d1ed3a92
Zach <zsam56@gmail.com>
2016-07-11 22:00:10 -0400

    adding product templates dynamically (using test prods for now)

--- eventpage.js
+++ eventpage.js
@@ -5,15 +5,32 @@ var currTemplate = "";
 chrome.browserAction.onClicked.addListener(function() {
   var path = chrome.extension.getURL('/');
   var iframe = document.getElementById('theFrame');
+  var prods = getProducts();
+
   var message = {
     command: 'render',
-    context: {thing: 'ShoRaq', path: path}
+    context: {thing: 'ShoRaq', path: path},
+    contextProd: {prodCount: prods.length,
+                  products: prods,
+                  path: path}
   };
   iframe.contentWindow.postMessage(message, '*');
 });
-function init() {
-  gapi.client.setApiKey('AIzaSyBhp6sBpEhRHUTY_SDj3E29q5RTdiWSFwo');
-  console.log("HEY");
+
+//returns a list of products as JSON objects
+function getProducts(){
+  var contextProducts = [
+    {name: 'Jennifer\'s glasses',
+      from: 'Joy',
+      cost: '500'},
+
+    {name: 'Bradley\'s suit',
+      from: 'Joy',
+      cost: '300'}
+  ];
+  //TODO- code to get the products given a videoID
+  //using test prods for now
+  return contextProducts;
 }
 
 // Event handler for the event from sandbox.html
